c20a47a7d533782f60b245398cfd37ac34f61c28
Ben Foxall <benfoxall@gmail.com>
2015-04-11 11:32:33 +0100

    tweet-stream :(

--- tweets.js
+++ tweets.js
@@ -1,22 +1,31 @@
 require('dotenv').load();
 
-var tweets = require('tweets');
+var Twitter = require('node-tweet-stream')
 
-var stream = new tweets({
+
+var stream = new Twitter({
   consumer_key:          process.env.TW_KEY,
   consumer_secret:       process.env.TW_SECRET,
-  access_token:          process.env.TW_TOKEN,
-  access_token_secret:   process.env.TW_TOKEN_SECRET
+  token:                 process.env.TW_TOKEN,
+  token_secret:          process.env.TW_TOKEN_SECRET
 });
 
 
 // temp
-stream.filter({track: 'pizza'})
+// stream.filter({track: 'pizza'})
+
+stream.track('nodejs')
+stream.track('pizza')
+
 
 stream.on('tweet', function(t){
   console.log(t.text)
 });
 
-stream.on('reconnect', function(reconnect){
-  console.log('reconnected', reconnect);
-});
+stream.on('error', function (err) {
+  console.log('Oh no')
+})
+
+// stream.on('reconnect', function(reconnect){
+//   console.log('reconnected', reconnect);
+// });
