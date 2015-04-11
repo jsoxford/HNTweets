62782ec7794b4fc8ea13d322f0def628cf75ae1a
Gil Gonçalves <lursty@gmail.com>
2015-04-11 14:46:44 +0100

    Updated links and changed urls structure

--- tweets.js
+++ tweets.js
@@ -12,49 +12,49 @@ var stream = new Twitter({
 
 
 var urls = [
-    {"href": "https://www.eff.org/press/releases/eff-busts-podcasting-patent-invalidating-key-claims-patent-office"},
-    {"href": "http://blog.rust-lang.org/2015/04/10/Fearless-Concurrency.html"},
-    {"href": "https://code.facebook.com/posts/1639473982937255/updating-our-open-source-patent-grant/"},
-    {"href": "http://quantombone.blogspot.com/2015/04/deep-learning-vs-probabilistic.html"},
-    {"href": "http://blog.easydns.org/2015/04/10/why-we-will-not-be-registering-easydns-sucks/"},
-    {"href": "https://mozilla.github.io/server-side-tls/ssl-config-generator/?1"},
-    {"href": "item?id=9356332"},
-    {"href": "https://blogs.janestreet.com/building-a-lower-latency-gc/"},
-    {"href": "http://jdh.hamkins.org/math-for-eight-year-olds"},
-    {"href": "http://educationware.net/introduction-to-linux-free-course-from-the-linux-foundation/"},
-    {"href": "http://blog.jgc.org/2015/04/the-one-line-you-should-add-to-every.html"},
-    {"href": "http://www.reenigne.org/blog/1k-colours-on-cga-how-its-done/"},
-    {"href": "item?id=9358843"},
-    {"href": "http://www.reenigne.org/blog/8088-pc-speaker-mod-player-how-its-done/"},
-    {"href": "https://tibastral.github.io/markdownify/"},
-    {"href": "https://gist.github.com/ImJasonH/c00cdd7aece6945fb8ea"},
-    {"href": "http://blog.jetbrains.com/dotnet/2015/04/10/introducing-resharper-cpp/"},
-    {"href": "http://home.web.cern.ch/about/updates/2015/04/first-successful-beam-record-energy-65-tev"},
-    {"href": "https://in.news.yahoo.com/the-slave-ship-that-ran-from-kerala-to-new-orleans-085329807.html"},
-    {"href": "http://www.iflscience.com/space/astronomical-quest-leads-ovens"},
-    {"href": "http://www.newstatesman.com/culture/2015/04/short-history-image-manipulation-photoshop"},
-    {"href": "http://www.newyorker.com/magazine/2005/09/19/the-lost-city-of-z"},
-    {"href": "http://my.teslamotors.com/models/design"},
-    {"href": "https://github.com/kgaughan/memoize.py"},
-    {"href": "http://www.rentever.com/"},
-    {"href": "http://sangaline.com/blog/optimizing_for_swype/"},
-    {"href": "https://github.com/bevacqua/hit-that"},
-    {"href": "http://www.bayesimpact.org/stories/?name=the-mob-the-money-and-the-mayhem"},
-    {"href": "http://www.theatlantic.com/technology/archive/2015/04/the-yo-yo-effect/389868?single_page=true"},
-    {"href": "http://thread.gmane.org/gmane.emacs.devel/185268"}
+    'http://www.filfre.net/2015/04/the-68000-wars-part-3-we-made-amiga-they-fucked-it-up/',
+    'http://thisisvoid.org/',
+    'https://dncmagazine.blob.core.windows.net/downloads/AngularCheatSheet-DNCMagazine.pdf',
+    'https://docs.google.com/document/d/1QZxArgMwidgCrAbuSikcB2iBxkffH6w0YB0C1qCsuH0',
+    'http://utcc.utoronto.ca/~cks/space/blog/linux/SystemdAndSyslog',
+    'http://www.nytimes.com/2015/04/08/opinion/yes-we-were-warned-about-ebola.html',
+    'http://www.devttys0.com/2015/04/hacking-the-d-link-dir-890l/',
+    'https://www.eff.org/press/releases/eff-busts-podcasting-patent-invalidating-key-claims-patent-office',
+    'http://blog.rust-lang.org/2015/04/10/Fearless-Concurrency.html',
+    'https://code.facebook.com/posts/1639473982937255/updating-our-open-source-patent-grant/',
+    'http://quantombone.blogspot.com/2015/04/deep-learning-vs-probabilistic.html',
+    'https://mozilla.github.io/server-side-tls/ssl-config-generator/?1',
+    'https://github.com/dullgiulio/perso/releases/tag/v0.1',
+    'http://blog.easydns.org/2015/04/10/why-we-will-not-be-registering-easydns-sucks/',
+    'item?id=9356332',
+    'http://jdh.hamkins.org/math-for-eight-year-olds',
+    'https://blogs.janestreet.com/building-a-lower-latency-gc/',
+    'http://educationware.net/introduction-to-linux-free-course-from-the-linux-foundation/',
+    'http://blog.jgc.org/2015/04/the-one-line-you-should-add-to-every.html',
+    'http://www.reenigne.org/blog/1k-colours-on-cga-how-its-done/',
+    'https://tibastral.github.io/markdownify/',
+    'http://www.reenigne.org/blog/8088-pc-speaker-mod-player-how-its-done/',
+    'item?id=9358843',
+    'https://gist.github.com/ImJasonH/c00cdd7aece6945fb8ea',
+    'http://home.web.cern.ch/about/updates/2015/04/first-successful-beam-record-energy-65-tev',
+    'http://blog.jetbrains.com/dotnet/2015/04/10/introducing-resharper-cpp/',
+    'https://in.news.yahoo.com/the-slave-ship-that-ran-from-kerala-to-new-orleans-085329807.html',
+    'http://www.newyorker.com/magazine/2005/09/19/the-lost-city-of-z',
+    'https://github.com/kgaughan/memoize.py',
+    'http://sangaline.com/blog/optimizing_for_swype/'
 ];
 
 var url = require('url');
 
 
-var urls_to_track = urls.map(function(obj) {
-    var parsed_url = url.parse(obj.href)
+var urls_to_track = urls.map(function(link) {
+    var parsed_url = url.parse(link)
     if ( parsed_url.host) {
         var hostname = parsed_url.hostname.replace('www.', '')
         var trimmed_url = hostname + parsed_url.path
     }
     else {
-        trimmed_url = 'news.ycombinator.com/' + obj.href
+        trimmed_url = 'news.ycombinator.com/' + link
     }
     return trimmed_url
 })
@@ -63,6 +63,7 @@ var urls_to_track = urls.map(function(obj) {
 })
 .join(',')
 
+console.log(urls_to_track);
 stream.track(urls_to_track);
 
 stream.on('tweet', function(t){
